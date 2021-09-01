ba4f4f1fa260752be59aaf8799e5d427ee799cae
Richard <richard.kim.0000@gmail.com>
2021-08-31 22:41:18 -0700

    Created the error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+// for the pseudo database command: npx json-server --watch data/db.json --port 8000
+
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
