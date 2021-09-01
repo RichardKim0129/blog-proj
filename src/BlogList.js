0805a2566981c0f1f90e7b1ecf472c741f20845f
Richard <richard.kim.0000@gmail.com>
2021-08-31 22:22:52 -0700

    Finishing up the delete button

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ agood, title }) => {
-// const BlogList = (props) => {
+  // const BlogList = (props) => {
   // const blogs = props.agood;
   // const title = props.title;
 
   return (
     <div className="blog-list">
-      <h2>{ title }</h2>
+      <h2>{title}</h2>
 
       {/* From the destructures parameter on line 1 */}
       {agood.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
